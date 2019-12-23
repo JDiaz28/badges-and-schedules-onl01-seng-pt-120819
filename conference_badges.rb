04f0ad1210abccd31ed7ae817f080e1c94d46eed
Abraham Moreno <adiaz2821@gmail.com>
2019-12-23 14:31:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,13 +9,12 @@ def batch_badge_creator(attendees)
   badges.push("Hello, my name is #{badge}.")
   end
   return badges
-  
 end 
 
 def assign_rooms(speakers)
   assign=[]
   
-  speakers.each_with_index{|speaker,index|
+  speakers.each_with_index{|speaker, index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
     }
    return assign
